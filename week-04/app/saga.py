6785f6b5d46bdb12844d0f62507df320aff61750
Valeria <lisenko.lera.17@mail.ru>
2026-02-24 13:12:16 +0700

    lab4

--- week-04/app/saga.py
+++ week-04/app/saga.py
@@ -4,5 +4,19 @@
 # и возвращать следующее состояние.
 
 def next_state(state: str, event: str) -> str:
-    # Ваш код здесь
-    raise NotImplementedError
+    if state == 'NEW':
+        if event == 'PAY_OK':
+            return 'PAID'
+        elif event in ('PAY_FAIL', 'cancel'):
+            return 'CANCELLED'
+        else:
+            return state
+    elif state == 'PAID':
+        if event == 'complete':
+            return 'DONE'
+        elif event in ('cancel', 'PAY_FAIL'):
+            return 'CANCELLED'
+        else:
+            return state
+    else:
+        return state
